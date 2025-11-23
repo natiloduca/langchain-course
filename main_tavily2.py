9e5179f316bb8c36a6f9b5fc541bab77dc13db87
Natalia Georgina Lo Duca <natalia.loduca@halopowered.com>
2025-11-23 10:31:33 -0300

    langchain course - tavily browser advance

---
+++ main_tavily2.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from schemas import AgentResponse
+
+tools = [TavilySearch()]
+llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=0,
+        verbose=True,
+    )
+
+
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    response_format=AgentResponse,
+)
+
+
+def main():
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+                }
+            ]
+        }
+    )
+    # Access structured response from the agent
+    structured = result.get("structured_response", None)
+    print(structured if structured is not None else result)
+
+
+if __name__ == "__main__":
+    main()
