0628423d7d5fdfc28d8264913d81307a1117403c
Natalia Georgina Lo Duca <natalia.loduca@halopowered.com>
2025-11-24 15:46:26 -0300

    langchain course - deep react agent - tool calling

---
+++ main_withoutool_calling_cap6.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain_groq import ChatGroq
+# from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+@tool
+def multiply(x: float, y: float) -> float:
+    """Multiply 'x' times 'y'."""
+    return x * y
+
+
+if __name__ == "__main__":
+    print("Hello Tool Calling")
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "you're a helpful assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
+    tools = [TavilySearch(), multiply]
+
+    # llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=0)
+    llm = ChatGroq(
+        model_name="llama-3.3-70b-versatile",
+        temperature=0.7,
+    )
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+    res = agent_executor.invoke(
+        {
+            "input": "what is the weather in dubai right now? compare it with San Fransisco, output should in in celsious",
+        }
+    )
+
+    print(res)
