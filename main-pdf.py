1daca02885d60d8ac60e2e97b1814290a06c5c95
Natalia Georgina Lo Duca <natalia.loduca@halopowered.com>
2025-11-27 13:34:59 -0300

    langchain course - RAG vector embeding with pdf

--- main-pdf.py
+++ main-pdf.py
@@ -7,8 +7,12 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_groq import ChatGroq
-from langchain_pinecone import PineconeVectorStore
+# from langchain_community.vectorstores import FAISS does not work in this environment
+from langchain_community.vectorstores import Annoy
+
 
 load_dotenv()
 
@@ -18,58 +22,37 @@ def format_docs(docs):
 
 
 if __name__ == "__main__":
-    print("Starting data retrieval with classic RetrievalQA chain...")
-
-    # --- 1. Define the SAME Embeddings Model used for ingestion ---
-    embeddings_model = HuggingFaceEmbeddings(
+    print("hi")
+    pdf_path = "react.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator="\n"
+    )
+    docs = text_splitter.split_documents(documents=documents)
+    embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    # --- 2. Define the LLM used for generating the final answer ---
+
+    vectorstore = Annoy.from_documents(docs, embeddings)
+    vectorstore.save_local("annoy_index_react")
+
+    new_vectorstore = Annoy.load_local(
+        "annoy_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     llm = ChatGroq(
         model_name="llama-3.3-70b-versatile",
         temperature=0.7,
     )
-
-    query = "what is Pinecone in machine learning?"
-    chain = PromptTemplate.from_template(template=query) | llm
-    # result = chain.invoke(input={})
-    # print(result.content)
-
-    # --- 3. Connect to the Pinecone Vector Store and get the retriever ---
-    vectorstore = PineconeVectorStore(
-        index_name=os.environ["INDEX_NAME"], embedding=embeddings_model
+    combine_docs_chain = create_stuff_documents_chain(
+        llm, retrieval_qa_chat_prompt
     )
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    retrival_chain = create_retrieval_chain(
-        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    retrieval_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combine_docs_chain
     )
 
-    result = retrival_chain.invoke(input={"input": query})
-
-    print(result)
-    template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    {context}
-    Question: {question}
-    Helpful Answer:"""
-    # prompt = PromptTemplate(input_variables=["context", "question"], template=template)
-    # chain = prompt | llm
-    # result = chain.invoke(input={"context": retrival_chain.invoke(input={"input": query}), "question": RunnablePassthrough()})
-    # res = rag_chain.invoke(input={"question": query})
-
-    custom_promt = PromptTemplate.from_template(template=template)
-
-    rag_chain = (
-        (
-            {
-                "context": vectorstore.as_retriever() | format_docs,
-                "question": RunnablePassthrough(),
-            }
-        )
-        | custom_promt
-        | llm
-    )
-    res = rag_chain.invoke(query)
-    print(res)
+    res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences"})
+    print(res["answer"])
